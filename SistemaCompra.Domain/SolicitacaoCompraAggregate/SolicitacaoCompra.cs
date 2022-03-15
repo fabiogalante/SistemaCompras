21ad01912281e8f354a4ce6d4da6e3267cdd5302
Fabio Galante <fabiogalantemans@gmail.com>
2022-03-15 15:14:37 -0300

    Add project files.

---
+++ SistemaCompra.Domain/SolicitacaoCompraAggregate/SolicitacaoCompra.cs
@@ -0,0 +1,53 @@
+﻿using SistemaCompra.Domain.Core;
+using SistemaCompra.Domain.Core.Model;
+using SistemaCompra.Domain.ProdutoAggregate;
+using SistemaCompra.Domain.SolicitacaoCompraAggregate.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemaCompra.Domain.SolicitacaoCompraAggregate
+{
+    public class SolicitacaoCompra : Entity
+    {
+        public UsuarioSolicitante UsuarioSolicitante { get; private set; }
+        public NomeFornecedor NomeFornecedor { get; private set; }
+        public IList<Item> Itens { get; private set; }
+        public DateTime Data { get; private set; }
+        public Money TotalGeral { get; private set; }
+        public Situacao Situacao { get; private set; }
+        public CondicaoPagamento CondicaoPagamento { get; set; }
+
+        private SolicitacaoCompra() { }
+
+        public SolicitacaoCompra(string usuarioSolicitante, string nomeFornecedor)
+        {
+            Id = Guid.NewGuid();
+            UsuarioSolicitante = new UsuarioSolicitante(usuarioSolicitante);
+            NomeFornecedor = new NomeFornecedor(nomeFornecedor);
+            Data = DateTime.Now;
+            Situacao = Situacao.Solicitado;
+        }
+
+        public void AdicionarItem(Produto produto, int qtde)
+        {
+            Itens.Add(new Item(produto, qtde));
+        }
+
+        public void RegistrarCompra(IEnumerable<Item> itens)
+        {
+            var items = itens.ToList();
+
+            if (!items.Any())
+                throw new BusinessRuleException("O total de itens de compra deve ser maior que 0");
+
+            var totalGeral = items.Sum(_ => _.Produto.Preco.Value);
+
+            if (totalGeral > 50000)
+            {
+                this.CondicaoPagamento.Valor = 30;
+            }
+
+        }
+    }
+}
